16fa861f7197f469141b3520db4f2435ab5b4c3a
Karl Swedberg <kswedberg@gmail.com>
2010-12-29 09:38:11 -0500

    make sure each container has a link before doing the biglink thing on it. trigger the first *visible* link within each container

--- jquery.biglinks.js
+++ jquery.biglinks.js
@@ -1,7 +1,7 @@
 /*!
- * jQuery Big Links plugin v1.0  
- * 
- * Date: 
+ * jQuery Big Links plugin v1.1
+ *
+ * Date: Wed Dec 29 09:35:09 2010 EST
  * Requires: jQuery v1.3.2+
  *
  * Copyright 2010, Karl Swedberg
@@ -15,27 +15,38 @@
 */
 
 (function($) {
+// avoid loading plugin twice
+if (typeof $.fn.biglinks === 'function') { return; }
 
+// biglinks plugin
 $.fn.biglinks = function(options) {
   var opts = $.extend({}, $.fn.biglinks.defaults, options || {});
-  
-  this.addClass(opts.biglinkClass);
-  
-  this.bind('click.biglinks', function(event) {
-    var $firstLink = $(this).find('a').eq(0);
-    
-    if (opts.preventDefault === true) {
-      event.preventDefault();
-      $firstLink.trigger('click');
-    } else if ( !$(event.target).closest('a').length ) {
-      window.location.href = $firstLink[0].href;
-    }
+
+  this.each(function() {
+    var $container = $(this);
+    // skip this container if it doesn't have a link
+    if ( !$container.find('a').length ) { return; }
+
+    $container.addClass(opts.biglinkClass);
+
+    $container.bind('click.biglinks', function(event) {
+      var $firstLink = $(this).find('a:visible').eq(0);
+
+      if (opts.preventDefault === true) {
+        event.preventDefault();
+        $firstLink.trigger('click');
+      } else if ( !$(event.target).closest('a').length ) {
+        window.location.href = $firstLink[0].href;
+      }
+
+    });
+
+    $container.bind('mouseenter.biglinks mouseleave.biglinks', function(event) {
+      $(this).toggleClass(opts.biglinkHoverClass, event.type === 'mouseenter');
+    });
+
   });
-  
-  this.bind('mouseenter.biglinks mouseleave.biglinks', function(event) {
-    $(this).toggleClass(opts.biglinkHoverClass, event.type === 'mouseenter');
-  });
-  
+
   return this;
 };
 
